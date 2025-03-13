6518b0f680630c0c952829d38374820fb1528bbb
gambino <e.set2283@gmail.com>
2025-03-13 22:41:45 +0300

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -4,7 +4,10 @@ function solveEquation(a, b, c) {
   let discriminant = b**2 - 4 * a * c;
 
   if (discriminant < 0) return arr;
-  else if (discriminant == 0) return arr.push((-b) / (2 * a));
+  else if (discriminant == 0) {
+    arr.push((-b) / (2 * a));
+    return arr;
+  } 
   else {
     arr.push((-b + Math.sqrt(discriminant) )/(2*a));
     arr.push((-b - Math.sqrt(discriminant) )/(2*a));
