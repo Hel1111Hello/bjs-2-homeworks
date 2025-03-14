899b427944a815a3d163507413e3f3acca7e0c5f
gambino <e.set2283@gmail.com>
2025-03-14 20:22:03 +0300

    Второе задание полностью готово

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,40 @@
 function getArrayParams(...arr) {
 
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+  let avg = (arr.reduce((a, b) => a + b, 0)) / arr.length;
+
+  avg = parseFloat(avg.toFixed(2));
+  
   return { min: min, max: max, avg: avg };
 }
 
 function summElementsWorker(...arr) {
 
+  return arr.reduce((a, b) => a + b, 0);
 }
 
 function differenceMaxMinWorker(...arr) {
 
+  if (arr.length == 0) return 0;
+  return Math.max(...arr) - Math.min(...arr);
 }
 
 function differenceEvenOddWorker(...arr) {
 
+  return arr.filter(num => num % 2 === 0).reduce((a, b) => a + b, 0) - arr.filter(num => num % 2 != 0).reduce((a, b) => a + b, 0);
 }
 
 function averageEvenElementsWorker(...arr) {
-
+  
+  return arr.filter(num => num % 2 === 0).reduce((a, b) => a + b, 0) / arr.filter(num => num % 2 === 0).length;
 }
 
 function makeWork (arrOfArr, func) {
+  
+  let arr = [];
 
+  for (let i = 0; i < arrOfArr.length; i++) arr.push(func(...arrOfArr[i]));
+
+  return  Math.max(...arr);
 }
