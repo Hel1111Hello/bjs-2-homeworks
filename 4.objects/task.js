2d5cb9d2ffe02e80b4122898f72812ce50f7cb55
gambino <e.set2283@gmail.com>
2025-03-21 13:26:50 +0300

    Четвёртое задание полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,30 @@
 function Student(name, gender, age) {
-  
+    this.age = age;
+    this.gender = gender;
+    this.name = name;
+
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (!this.marks || !Array.isArray(this.marks)) return;
+
+    this.marks.push(...marks);
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (!this.marks || this.marks.length == 0) return 0;
+
+    return this.marks.reduce((el1, el2) => el1 + el2, 0) / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+
+    this.excluded = reason;
 }
